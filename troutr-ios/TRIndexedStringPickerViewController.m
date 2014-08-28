9296379997c770533cbaa27783bfd1e151c23045
worace <horace.d.williams@gmail.com>
2014-08-27 22:45:05 -0400

    add search bar to fly / fish picker

--- troutr-ios/TRIndexedStringPickerViewController.m
+++ troutr-ios/TRIndexedStringPickerViewController.m
@@ -8,10 +8,38 @@
 
 #import "TRIndexedStringPickerViewController.h"
 
+@interface TRIndexedStringPickerViewController () <UISearchBarDelegate>
+@property (nonatomic, strong) UISearchBar *searchBar;
+@property (nonatomic, strong) NSMutableArray *filteredResults;
+@property BOOL isSearching;
+@end
+
 @implementation TRIndexedStringPickerViewController
+
+#pragma mark - search bar delegate
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if ([searchText length] == 0) {
+        self.isSearching = NO;
+    } else {
+        NSLog(@"total items: %d", [[[self store] sortedList] count]);
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains[c] %@",searchText];
+        self.filteredResults = [NSMutableArray arrayWithArray:[[[self store] sortedList] filteredArrayUsingPredicate:predicate]];
+        self.isSearching = YES;
+    }
+    [self.tableView reloadData];
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"cancel");
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 70, 320, 44)];
+    self.searchBar.delegate = self;
+    self.filteredResults = [[NSMutableArray alloc] init];
+    [self.tableView setTableHeaderView:self.searchBar];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
 }
 
@@ -22,20 +50,36 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return [[self.store indexKeys] count];
+    if (self.isSearching) {
+        return 1;
+    } else {
+        return [[self.store indexKeys] count];
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSString *key = [[self.store indexKeys] objectAtIndex:section];
-    return [[self.store itemsForKey:key] count];
+    if (self.isSearching) {
+        return [self.filteredResults count];
+    } else {
+        NSString *key = [[self.store indexKeys] objectAtIndex:section];
+        return [[self.store itemsForKey:key] count];
+    }
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return [[self.store indexKeys] objectAtIndex:section];
+    if (self.isSearching) {
+        return @"";
+    } else {
+        return [[self.store indexKeys] objectAtIndex:section];
+    }
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
-    return [self alphabet];
+    if (self.isSearching) {
+        return @[];
+    } else {
+        return [self alphabet];
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -60,7 +104,11 @@
 }
 
 - (NSString *)itemForIndexPath:(NSIndexPath *)indexPath {
-    return [[self itemsForIndexPath:indexPath] objectAtIndex:indexPath.row];
+    if (self.isSearching) {
+        return [self.filteredResults objectAtIndex:indexPath.row];
+    } else {
+        return [[self itemsForIndexPath:indexPath] objectAtIndex:indexPath.row];
+    }
 }
 
 - (NSArray *)itemsForIndexPath:(NSIndexPath *)indexPath {
